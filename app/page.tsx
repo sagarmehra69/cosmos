81b2e972cc96689426f1de07d6cb45068294d436
sagarmehra69 <3568sagarmehra@gmail.com>
2026-07-02 14:17:13 +0530

    feat: initialize Project Cosmos with Next.js

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar/navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+
+      <h1>Project Cosmos 🚀</h1>
+    </main>
+  );
+}
---
+++ components/Navbar/navbar.tsx
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <h2>🚀 Cosmos</h2>
+
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Experience</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
